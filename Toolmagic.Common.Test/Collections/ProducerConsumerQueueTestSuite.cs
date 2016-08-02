c95847bc86aeaeb53e304e69c03d10685699f54d
Vladimir Belov <vladimir.belov@kaspersky.com>
2016-08-02 21:35:47 +0700

    Added ProducerConsumerQueue.

---
+++ Toolmagic.Common.Test/Collections/ProducerConsumerQueueTestSuite.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Toolmagic.Common.Collections;
+
+namespace Toolmagic.Common.Test.Collections
+{
+	[TestFixture]
+	public sealed class ProducerConsumerQueueTestSuite
+	{
+		[Test]
+		public void ProducerConsumerFailsOnNullConsumerActiontest()
+		{
+			const Action<string> nullAction = null;
+
+			var exception = Assert.Throws<ArgumentNullException>
+				(
+					() => ProducerConsumerQueue<string>.Start(nullAction)
+				);
+
+			Assert.AreEqual("consumerAction", exception.ParamName);
+		}
+
+		[Test]
+		public void ProducerConsumerQueueTest()
+		{
+			const int initialItemCount = 100;
+			const int producerDelay = 10;
+			const int consumerDelay = producerDelay/5;
+
+			var processedItems = new ConcurrentBag<int>();
+
+			using (var queue = ProducerConsumerQueue<int>.Start(value =>
+			{
+				processedItems.Add(value);
+				System.Console.WriteLine(value);
+				Thread.Sleep(producerDelay);
+			}))
+			{
+				Parallel.ForEach
+					(
+						Enumerable.Range(0, initialItemCount - 1),
+						new ParallelOptions {MaxDegreeOfParallelism = Environment.ProcessorCount},
+						value =>
+						{
+							// ReSharper disable once AccessToDisposedClosure
+							queue.Enqueue(value);
+							Thread.Sleep(consumerDelay);
+						}
+					);
+
+				Thread.Sleep(500);
+				queue.Shutdown();
+			}
+
+			Assert.Greater(initialItemCount, processedItems.Count);
+		}
+
+		[Test]
+		public void ProducerConsumerStartsTest()
+		{
+			using (var queue = ProducerConsumerQueue<string>.Start(value => { }))
+			{
+				Assert.IsNotNull(queue);
+			}
+		}
+	}
+}
---
+++ Toolmagic.Common/Collections/ProducerConsumerQueue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Toolmagic.Common.Collections
+{
+	public sealed class ProducerConsumerQueue<T> : IDisposable
+	{
+		private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+		private readonly Action<T> _consumerAction;
+		private readonly Thread _consumerThread;
+		private readonly object _lockObject = new object();
+		private readonly Queue<T> _queue = new Queue<T>();
+
+		private ProducerConsumerQueue(Action<T> consumerAction)
+		{
+			Argument.IsNotNull(consumerAction, nameof(consumerAction));
+
+			_consumerAction = consumerAction;
+			_consumerThread = new Thread(Consume);
+		}
+
+		public void Dispose()
+		{
+			Shutdown();
+		}
+
+		public static ProducerConsumerQueue<T> Start(Action<T> consumerAction)
+		{
+			var queue = new ProducerConsumerQueue<T>(consumerAction);
+			queue.Start();
+			return queue;
+		}
+
+		private void Start()
+		{
+			_consumerThread.Start();
+		}
+
+		public void Shutdown()
+		{
+			_cancellationTokenSource.Cancel();
+			lock (_lockObject)
+			{
+				Monitor.Pulse(_lockObject);
+			}
+			_consumerThread.Join();
+		}
+
+		public void Enqueue(T item)
+		{
+			lock (_lockObject)
+			{
+				_queue.Enqueue(item);
+				Monitor.Pulse(_lockObject);
+			}
+		}
+
+		private void Consume()
+		{
+			while (!_cancellationTokenSource.IsCancellationRequested)
+			{
+				T item;
+				lock (_lockObject)
+				{
+					while (_queue.Count == 0)
+					{
+						Monitor.Wait(_lockObject);
+					}
+
+					item = _queue.Dequeue();
+				}
+
+				if (item == null)
+				{
+					return;
+				}
+
+				_consumerAction.Invoke(item);
+			}
+		}
+	}
+}
